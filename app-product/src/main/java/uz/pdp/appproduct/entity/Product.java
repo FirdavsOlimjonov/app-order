394abd13b04224b2a78e0ab209a70142a4b672b0
king5740 <utolibjonov24@gmail,com>
2022-09-28 08:56:42 +0500

    product

---
+++ app-product/src/main/java/uz/pdp/appproduct/entity/Product.java
@@ -0,0 +1,28 @@
+package uz.pdp.appproduct.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private boolean isValid;
+
+    private float price;
+
+    private String name;
+
+    public Product(Integer id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    private Category category;
+}
