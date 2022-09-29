c8494a233c1b43f8137c08d07e39629f994ab8a3
firdavs <firdavsolimjonov25@gmail.com>
2022-09-29 16:52:04 +0500

    firdavs

--- app-order/src/main/java/uz/pdp/apporder/payload/OrderChartDTO.java
+++ app-order/src/main/java/uz/pdp/apporder/payload/OrderChartDTO.java
@@ -4,9 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import uz.pdp.apporder.entity.Branch;
+import uz.pdp.apporder.entity.enums.OrderStatusEnum;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -15,13 +22,16 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderChartDTO {
 
-    private Map<Date,Integer> intervalData;
+    private List<Integer> intervalData;
 
-    private String branch;
-
-    @NotNull
-    private Date fromDate;
+    private Integer branchId;
 
     @NotNull
-    private Date tillDate;
+    private LocalDate fromDate;
+
+    @NotNull
+    private LocalDate tillDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatusEnum orderStatusEnum;
 }
--- app-order/src/main/java/uz/pdp/apporder/repository/OrderRepository.java
+++ app-order/src/main/java/uz/pdp/apporder/repository/OrderRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.apporder.entity.Order;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+    List<Order> findAllByStatusEnumContainsIgnoreCase(String status);
 }
