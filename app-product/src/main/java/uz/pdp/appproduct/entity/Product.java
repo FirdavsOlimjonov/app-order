741a72b5a11e6125d6e29a16829cafa79c320032
firdavs <firdavsolimjonov25@gmail.com>
2022-10-04 08:13:27 +0500

    CheckAuthEmpl

--- app-product/src/main/java/uz/pdp/appproduct/entity/Product.java
+++ app-product/src/main/java/uz/pdp/appproduct/entity/Product.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Setter
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private boolean active;
