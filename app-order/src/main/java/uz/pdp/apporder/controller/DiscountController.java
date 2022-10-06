d1b80a071910a37d14cfdc2d7a0e5fc77e9d189c
yorqinjon <yorqinjonmahmudov93@gmail.com>
2022-10-06 17:20:54 +0500

    starting discounts

---
+++ app-order/src/main/java/uz/pdp/apporder/controller/DiscountController.java
@@ -0,0 +1,10 @@
+package uz.pdp.apporder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping()
+public interface DiscountController {
+    String BASE_PATH = "discount";
+
+
+}
